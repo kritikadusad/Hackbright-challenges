12acf9196ba50ea63044e03ebbce27a7da985824
Kritika Dusad <kritikadusad@gmail.com>
2019-01-02 12:52:13 -0800

    Solved.

---
+++ Hackbright-challenges/binary-search/binarysearch.py
@@ -0,0 +1,53 @@
+"""Using a binary search, find val in a range of 1-100. Return # of guesses.
+
+Construct a list of 1-100 (inclusive). Write a binary search that searches
+for val in that list (val will always be a number between 1 and 100).
+
+Return the number of searches it took to find val. For a proper binary search
+of 1-100, this should never be more than 7.
+
+    >>> binary_search(50)
+    1
+
+    >>> binary_search(25)
+    2
+
+    >>> binary_search(75)
+    2
+
+    >>> binary_search(31) <= 7
+    True
+
+    >>> max([binary_search(i) for i in range(1, 101)])
+    7
+"""
+
+
+def binary_search(val):
+    """Using binary search, find val in range 1-100. Return # of guesses."""
+
+    assert 0 < val < 101, "Val must be between 1-100"
+    num_guesses = 0
+
+    low = 0
+    high = 100
+    mid = int((low + high) / 2)
+
+    while val != mid:
+
+        if val > mid:
+            low = mid
+        else:
+            high = mid
+        mid = int((low + high) / 2)
+        num_guesses += 1
+
+    num_guesses += 1
+
+    return num_guesses
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE TERRIFIC AT THIS!\n")
