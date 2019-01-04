46b6f6fc31d663eb4b2d494576252d8d23c72939
Kritika Dusad <kritikadusad@gmail.com>
2019-01-04 11:12:26 -0800

    solved.

---
+++ Hackbright-challenges/recursive-index/recursiveindex.py
@@ -0,0 +1,45 @@
+"""Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+
+For example:
+
+    >>> recursive_index(5, [1, 3, 5, 2, 4])
+    2
+
+    >>> recursive_index("hey", ["hey", "there", "you"])
+    0
+
+    >>> recursive_index("you", ["hey", "there", "you"])
+    2
+
+    >>> recursive_index("zork", ["foo", "bar", "baz"]) is None
+    True
+
+"""
+
+
+def recursive_index(needle, haystack):
+    """Given list (haystack), return index (0-based) of needle in the list.
+
+    Return None if needle is not in haystack.
+
+    Do this with recursion. You MAY NOT USE A `for` OR `while` LOOP.
+    """
+    if needle not in set(haystack):
+        return
+    else:
+        index = 0
+        if needle == haystack[0]:
+            index = 0
+        else:
+            index = 1 + recursive_index(needle, haystack[1:])
+        return index
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. GO GO GO!\n")
