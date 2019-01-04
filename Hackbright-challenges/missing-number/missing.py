af673373324df9c919c6c018eee0b2a609c77682
Kritika Dusad <kritikadusad@gmail.com>
2019-01-03 16:01:59 -0800

    missing-number problem added.

---
+++ Hackbright-challenges/missing-number/missing.py
@@ -0,0 +1,19 @@
+"""Given a list of numbers 1...max_num, find which one is missing in a list."""
+
+
+def missing_number(nums, max_num):
+    """Given a list of numbers 1...max_num, find which one is missing.
+
+    *nums*: list of numbers 1..[max_num]; exactly one digit will be missing.
+    *max_num*: Largest potential number in list
+
+    >>> missing_number([7, 3, 2, 4, 5, 6, 1, 9, 10], 10)
+    8
+    
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASS. NICELY DONE!\n")
