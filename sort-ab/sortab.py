ef2b2ca123e750f8c0e19afd7c0ddc1ddfca4b1a
Kritika Dusad <kritikadusad@gmail.com>
2019-01-05 15:50:29 -0800

    solved.

---
+++ sort-ab/sortab.py
@@ -0,0 +1,47 @@
+"""Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+You may not use sorted() or .sort().
+
+Check edge cases of empty lists:
+
+    >>> sort_ab([], [])
+    []
+
+    >>> sort_ab([1, 2,3], [])
+    [1, 2, 3]
+
+    >>> sort_ab([], [1, 2, 3])
+    [1, 2, 3]
+
+Check:
+
+    >>> sort_ab([1, 3, 5, 7], [2, 6, 8, 10])
+    [1, 2, 3, 5, 6, 7, 8, 10]
+"""
+
+
+def sort_ab(a, b):
+    """Given already-sorted lists, `a` and `b`, return sorted list of both.
+
+    You may not use sorted() or .sort().
+    """
+    sorted_lst = []
+    while a and b:
+        if a[0] < b[0]:
+            sorted_lst.append(a[0])
+            a.pop(0)
+        else:
+            sorted_lst.append(b[0])
+            b.pop(0)
+    if a:
+        sorted_lst.extend(a)
+    else:
+        sorted_lst.extend(b)
+
+    return sorted_lst
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. YOU'RE A MERGE CHAMPION!!\n")
