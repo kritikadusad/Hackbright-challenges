e8606aaa5fc7e6405f0d3786dca5a20178d21429
Kritika Dusad <kritikadusad@gmail.com>
2019-02-19 10:45:18 -0800

    solved.

---
+++ hexconvert.py
@@ -0,0 +1,56 @@
+"""Convert a hexadecimal string, like '1A', into it's decimal equivalent.
+
+    >>> hex_convert('6')
+    6
+
+    >>> hex_convert('10')
+    16
+
+    >>> hex_convert('FF')
+    255
+
+    >>> hex_convert('FFFF')
+    65535
+"""
+
+
+def hex_convert(hex_in):
+    """Convert a hexadecimal string, like '1A', into it's decimal equivalent."""
+    hex_dict = {
+        "0": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "A": 10,
+        "B": 11,
+        "C": 12,
+        "D": 13,
+        "E": 14,
+        "F": 15
+    }
+
+    hex_in_lst = list(hex_in)
+
+    counter = 0
+    decimal = []
+    while hex_in_lst:
+        current = hex_in_lst.pop()
+
+        num = hex_dict[current] * (16 ** counter)
+        decimal.append(num)
+
+        counter += 1
+
+    return sum(decimal)
+
+
+if __name__ == "__main__":
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASS. YOU'RE TERRIFIC!\n")
