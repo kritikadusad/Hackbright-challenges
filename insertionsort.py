48636bfd74bc202bd2872916a09311a2179889d5
Kritika Dusad <kritikadusad@gmail.com>
2019-02-19 11:36:03 -0800

    solved.

---
+++ insertionsort.py
@@ -0,0 +1,42 @@
+"""Given a list, sort it using insertion sort.
+
+For example::
+
+    >>> from random import shuffle
+    >>> alist = list(range(1, 11))
+
+    >>> shuffle(alist)
+    >>> insertion_sort(alist)
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    >>> shuffle(alist)
+    >>> insertion_sort(alist)
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    >>> shuffle(alist)
+    >>> insertion_sort(alist)
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+"""
+
+
+def insertion_sort(alist):
+    """Given a list, sort it using insertion sort."""
+
+    i = 1
+    while i < len(alist):
+        j = 0
+        while j < i:
+            if alist[i] < alist[j]:
+                temp = alist[j]
+                alist[j] = alist[i]
+                alist[i] = temp
+            j += 1
+
+        i += 1
+    return alist
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n*** ALL TESTS PASSED. NICE SORTING!\n")
